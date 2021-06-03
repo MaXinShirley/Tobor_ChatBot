8f59e57cd8428351952862383f26d29441f18b58
MaXinShirley <ma.xin@eonreality.com>
2021-06-03 12:15:24 +0800

    Add events script for animation play

---
+++ Assets/!Scripts/Events.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public static class Events
+{
+    public static Action<int> AnimationSelected;
+    public static void OnSelectAnimation(int index) => AnimationSelected?.Invoke(index);
+
+    public static Action<int> ExpressionSelected;
+    public static void OnSelectExpression(int index) => ExpressionSelected?.Invoke(index);
+}
